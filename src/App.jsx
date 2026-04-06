de545cdf95ca8f026f98bcc4eafb7f1012979e0e
joaoehrich <joaoehrich@gmail.com>
2026-04-06 13:59:46 -0300

    versão final do projeto com auth e supabase

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Auth from './pages/auth'
+
+function App() {
+  return <Auth />
+}
+
+export default App
---
+++ src/pages/auth.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { supabase } from '../api/supabaseClient'
+
+function Auth() {
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+  const [modoLogin, setModoLogin] = useState(true)
+
+  async function handleAuth(e) {
+    e.preventDefault()
+
+    if (modoLogin) {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password: senha
+      })
+
+      if (error) {
+        alert(error.message)
+      } else {
+        alert('Login realizado!')
+      }
+
+    } else {
+      const { error } = await supabase.auth.signUp({
+        email,
+        password: senha
+      })
+
+      if (error) {
+        alert(error.message)
+      } else {
+        alert('Conta criada!')
+      }
+    }
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>{modoLogin ? 'Login' : 'Cadastro'}</h2>
+
+      <form onSubmit={handleAuth}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        <br /><br />
+
+        <button type="submit">
+          {modoLogin ? 'Entrar' : 'Cadastrar'}
+        </button>
+      </form>
+
+      <br />
+
+      <button onClick={() => setModoLogin(!modoLogin)}>
+        {modoLogin ? 'Criar conta' : 'Já tenho conta'}
+      </button>
+    </div>
+  )
+}
+
+export default Auth
